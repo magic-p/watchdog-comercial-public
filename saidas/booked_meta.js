9b01d7c4c29beff6b22d59ff7cf081bec99e2fcc
Pedro <pedroconcy@gmail.com>
2026-02-23 18:23:52 -0300

    watchdog: initial deploy

---
+++ saidas/booked_meta.js
@@ -0,0 +1,65 @@
+window.WATCHDOG_BOOKED_META = {
+  "month": "Fev",
+  "target": 10619324,
+  "buckets": [
+    {
+      "label": "Renovacao Particular +5k",
+      "target": 7954618,
+      "schools_target": 570,
+      "rr_target": 323,
+      "rr_sdr_dia_target": 1.3
+    },
+    {
+      "label": "Renovacao Publica +5k",
+      "target": 390828,
+      "schools_target": 46,
+      "rr_target": 32,
+      "rr_sdr_dia_target": 1.94
+    },
+    {
+      "label": "Renovacao Particular -5k",
+      "target": 319151,
+      "schools_target": 97,
+      "rr_target": 69,
+      "rr_sdr_dia_target": 2.05
+    },
+    {
+      "label": "Anos Anteriores",
+      "target": 726605,
+      "schools_target": 92,
+      "rr_target": 65,
+      "rr_sdr_dia_target": 0.65
+    },
+    {
+      "label": "Concorrencia",
+      "target": 262706,
+      "schools_target": 30,
+      "rr_target": 16,
+      "rr_sdr_dia_target": 0.3
+    },
+    {
+      "label": "Retracao",
+      "target": 440057,
+      "schools_target": 53,
+      "rr_target": 36,
+      "rr_sdr_dia_target": 0.53
+    },
+    {
+      "label": "Novas",
+      "target": 525359,
+      "schools_target": 65,
+      "rr_target": 66,
+      "rr_sdr_dia_target": 0.25
+    }
+  ],
+  "wins": [],
+  "kpis": {
+    "meta_anual_escolas": 3736,
+    "meta_mensal_escolas": 953,
+    "sdr_meta_pct": 12.0,
+    "efetividade_sdr_pct": null,
+    "zombie_days": 3,
+    "campanha_taxa_verde": 2.0,
+    "campanha_taxa_azul": 1.0
+  }
+};
