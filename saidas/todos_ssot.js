5dfb629c6aba925b68089428b9a54d873f6f890d
magic-p <pedro.concy@estantemagica.com.br>
2026-03-19 10:15:36 +0100

    watchdog: update 2026-03-19 09:15

--- saidas/todos_ssot.js
+++ saidas/todos_ssot.js
@@ -2756,5 +2756,19 @@ window.WATCHDOG_TODOS = [
     "priorityRank": null,
     "updatedAt": "2026-03-18T21:47:00",
     "completedAt": null
+  },
+  {
+    "id": "t207",
+    "task": "Teste - Apagar depois",
+    "comment": "é somente um teste de tarefa",
+    "owner": "Pedro",
+    "date": "20/mar",
+    "tier": "t1",
+    "tierLabel": "A fazer",
+    "warn": null,
+    "done": false,
+    "priorityRank": null,
+    "updatedAt": null,
+    "completedAt": null
   }
 ];
