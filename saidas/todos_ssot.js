0433bc9a7ea8507e7ea0218bb03a38b62796d1d5
magic-p <pedro.concy@estantemagica.com.br>
2026-03-19 20:17:34 +0100

    watchdog: update 2026-03-19 19:17

--- saidas/todos_ssot.js
+++ saidas/todos_ssot.js
@@ -2861,12 +2861,12 @@ window.WATCHDOG_TODOS = [
     "comment": "Tarefa criada conforme solicitado pelo usuário",
     "owner": "Pedro",
     "date": "20/mar",
-    "tier": "done",
-    "tierLabel": "Concluido",
+    "tier": "t2",
+    "tierLabel": "Fazendo",
     "warn": null,
-    "done": true,
+    "done": false,
     "priorityRank": null,
     "updatedAt": "2026-03-19T19:17:00",
-    "completedAt": "2026-03-19T19:17:00"
+    "completedAt": null
   }
 ];
