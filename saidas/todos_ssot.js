adaa40841247b682f888680b11691989a3d5b6fc
magic-p <pedro.concy@estantemagica.com.br>
2026-03-19 19:23:15 +0100

    watchdog: update 2026-03-19 18:23

--- saidas/todos_ssot.js
+++ saidas/todos_ssot.js
@@ -2854,5 +2854,19 @@ window.WATCHDOG_TODOS = [
     "priorityRank": null,
     "updatedAt": "2026-03-19T17:58:00",
     "completedAt": null
+  },
+  {
+    "id": "t212",
+    "task": "Criar nova campanha de marketing",
+    "comment": "Campanha para próxima segunda-feira",
+    "owner": "Pedro",
+    "date": "23/mar",
+    "tier": "t1",
+    "tierLabel": "A fazer",
+    "warn": null,
+    "done": false,
+    "priorityRank": null,
+    "updatedAt": "2026-03-19T18:23:00",
+    "completedAt": null
   }
 ];
