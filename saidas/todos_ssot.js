0bcfa84c317111c2d8957e1041f1d3724dd76722
magic-p <pedro.concy@estantemagica.com.br>
2026-03-25 12:59:00 +0100

    watchdog: update 2026-03-25 11:58

--- saidas/todos_ssot.js
+++ saidas/todos_ssot.js
@@ -3034,7 +3034,7 @@ window.WATCHDOG_TODOS = [
     "warn": null,
     "done": false,
     "priorityRank": null,
-    "updatedAt": "2026-03-25T00:00:00",
+    "updatedAt": null,
     "completedAt": null
   },
   {
@@ -3048,7 +3048,7 @@ window.WATCHDOG_TODOS = [
     "warn": null,
     "done": false,
     "priorityRank": null,
-    "updatedAt": "2026-03-25T00:00:00",
+    "updatedAt": null,
     "completedAt": null
   },
   {
@@ -3062,7 +3062,7 @@ window.WATCHDOG_TODOS = [
     "warn": null,
     "done": false,
     "priorityRank": null,
-    "updatedAt": "2026-03-25T00:00:00",
+    "updatedAt": null,
     "completedAt": null
   }
 ];
